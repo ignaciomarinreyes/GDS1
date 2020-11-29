c819ab7368aba2a4cd9b5a177c8fe3104af31b61
SPTSaulo <saulobach1@gmail.com>
2020-11-29 20:49:51 +0000

    Probando implementacion API Rest

--- gs1pr/DrawYourRouteBackEnd/DrawYourRouteAPI/src/main/java/com/gs1/drawyourrouteapi/resources/JavaEE8Resource.java
+++ gs1pr/DrawYourRouteBackEnd/DrawYourRouteAPI/src/main/java/com/gs1/drawyourrouteapi/resources/JavaEE8Resource.java
@@ -1,18 +1,31 @@
 package com.gs1.drawyourrouteapi.resources;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.Persona;
 
 /**
  *
- * @author 
+ * @author
  */
-@Path("javaee8")
+@Path("/map")
 public class JavaEE8Resource {
     
     @GET
+    @Path("/saludo")
+    @Produces(MediaType.APPLICATION_JSON)
     public String dimeHola() {
-        return "Hoola";
+        return "Hola";
+    }
+    
+    @POST
+    @Path("/adios")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String dimeAdios() {
+        return "Adios";
     }
 }
