338956ee7340930f69f7493d31f157beca9b9bb3
SPTSaulo <saulobach1@gmail.com>
2020-11-30 17:46:28 +0000

    Usuarios implementados API

--- gs1pr/DrawYourRouteBackEnd/DrawYourRoute/src/main/java/model/Persona.java
+++ gs1pr/DrawYourRouteBackEnd/DrawYourRoute/src/main/java/model/Persona.java
@@ -3,12 +3,19 @@ package model;
 public class Persona {
     private String nombre;
     private int edad;
+    private String contraseña;
+    private int id;
 
-    public Persona(String nombre, int edad) {
+    public Persona(String nombre, int edad, String contraseña, int id) {
         this.nombre = nombre;
         this.edad = edad;
+        this.contraseña = contraseña;
+        this.id = id;
     }
-
+    
+    public int getId() {
+        return id;
+    }
     public String getNombre() {
         return nombre;
     }
@@ -23,5 +30,14 @@ public class Persona {
 
     public void setEdad(int edad) {
         this.edad = edad;
+    }
+    
+    @Override
+    public String toString() {
+        return "Persona: " + nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
     }
 }
